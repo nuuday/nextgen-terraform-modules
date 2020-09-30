605a31d0de0e36ce3d24842956881f3e4874851b
eikc <eic@nuuday.dk>
2020-09-30 11:39:20 +0200

    feat: added RDS module

---
+++ modules/ecr/main.tf
@@ -0,0 +1,81 @@
+resource "aws_ecr_repository" "repository" {
+  name                 = var.repository_name
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+  tags = var.common_tags
+}
+
+resource "aws_ecr_repository_policy" "policy" {
+  repository = aws_ecr_repository.repository.name
+  policy     = <<EOF
+    {
+      "Statement": [
+        {
+          "Action": [
+            "ecr:BatchGetImage",
+            "ecr:GetDownloadUrlForLayer",
+            "ecr:GetAuthorizationToken"
+          ],
+          "Principal": {
+            "AWS": [
+              "${var.principal}"
+            ]
+          },
+          "Effect": "Allow",
+          "Sid": "Allow pull"
+        }
+      ],
+      "Version": "2008-10-17"
+    }
+    EOF
+}
+
+resource "aws_ecr_lifecycle_policy" "this" {
+  policy = jsonencode({
+    rules = [
+      {
+        rulePriority = 1
+        description  = "keep the last 5 production images"
+        selection = {
+          tagStatus     = "tagged"
+          tagPrefixList = ["v"]
+          countType     = "imageCountMoreThan"
+          countNumber   = 5
+        }
+        action = {
+          type = "expire"
+        }
+      },
+      {
+        rulePriority = 5
+        description  = "keep the last 5 stable images"
+        selection = {
+          tagStatus     = "tagged"
+          tagPrefixList = ["stable-"]
+          countType     = "imageCountMoreThan"
+          countNumber   = 5
+        }
+        action = {
+          type = "expire"
+        }
+      },
+      {
+        rulePriority = 100
+        description  = "other images"
+        selection = {
+          tagStatus   = "any"
+          countType   = "imageCountMoreThan"
+          countNumber = 15
+        }
+        action = {
+          type = "expire"
+        }
+      },
+    ]
+  })
+
+  repository = aws_ecr_repository.repository.name
+}
---
+++ modules/rds/variables.tf
@@ -0,0 +1,50 @@
+variable "source_security_group" {}
+
+variable "vpc_id" {}
+
+variable "database_instance_type" {}
+
+variable "database_storage_size" {}
+
+variable "name" {}
+
+variable "user" {}
+
+variable "password" {}
+
+variable "tags" {
+  type = map(string)
+}
+
+variable "subnet_ids" {
+  type = list(string)
+}
+
+variable "engine_version" {
+  type = string
+}
+
+variable "major_engine_version" {
+  type = string
+}
+
+variable "auto_minor_version_upgrade" {
+  type = bool
+  default = true
+}
+
+variable "family" {
+  type = string
+}
+
+variable "maintenance_window" {
+  type = string
+}
+
+variable "backup_window" {
+  type = string
+}
+
+variable "backup_retention_period" {
+  type = number
+}
